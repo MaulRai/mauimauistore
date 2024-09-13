aeb81e61f8782882374158c08d78e64ad0992bbd
MaulRai <muhammadraihanofc@gmail.com>
2024-09-13 15:09:22 +0700

    integrate product sama database, udah works

--- main/models.py
+++ main/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator 
+import uuid
 
 class MoodEntry(models.Model):
     mood = models.CharField(max_length=255)
@@ -11,11 +13,13 @@ class MoodEntry(models.Model):
         return self.mood_intensity > 5
     
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=63)
     price = models.IntegerField()
     rating = models.IntegerField()
     stock = models.IntegerField()
     desc = models.CharField(max_length=255)
+    time = models.DateField(auto_now_add=True)
     
     @property
     def get_formatted_price(self):
